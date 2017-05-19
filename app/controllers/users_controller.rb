5744914b5076062a287f95b1222118380f6198a4
toshimasa <masaismasa@gmail.com>
2017-05-19 14:21:15 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+
   def index
     @users = User.all.page(params[:page])
   end
@@ -28,4 +30,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  
+end
